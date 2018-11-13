06768f3acf641e12b129ef52ce97a88057105b55
wei guo <wei.guo@ge1.iut-cachan.u-psud.fr>
2018-11-13 11:39:10 +0100

    exo2

--- src/main.c
+++ src/main.c
@@ -2,8 +2,21 @@
 //
 
 #include "my_header.h"
+#include< math.>"
 
+#define ECART 1e-10
 int main(void)
 {
+    double a = 1.0, b=2.0, c=3.0;
+    double delta;
+    unsigned char nb_solutions;
+
+    delta = b*b- 4.0 * a *c;
+    if (_fabs(delta)<ECART)
+    nb_solutions=1;
+    else if(delta>0)
+    nb_solutions=2;
+    else
+    nb_solutions=0
     return 0;
 }
