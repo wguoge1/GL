997bbb1d7ea47315c62747e09ce41e6a5f5d66c0
wguoge1 <44798726+wguoge1@users.noreply.github.com>
2018-11-14 23:03:22 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -3,7 +3,39 @@
 
 #include "my_header.h"
 
+
 int main(void)
 {
-    return 0;
+	int s1=0, n1=3, n2=3, n3=0,b=0 ,c=1 ,s4=0, a=1 ,b1=1;
+	long unsigned int s2=0;
+	while(n1<36)
+	 {
+		 s1= s1+n1;
+		 n1= n1+3;
+     }
+	while(n2<= 9876)
+	{
+		s2= s2+n2;
+    	n2= n2+3;
+	}
+	while(c<=12)
+	{
+		b=rand() %10+1;
+		s3=s3+b;
+		c++;
+	}
+	while(b1<=20)
+	{
+		if(a%3==0 || a%7==0)
+		{
+			s4= s4+a;
+			b1++;
+		}
+		
+	}
+	return 0;
+	
 }
+		
+
+    
