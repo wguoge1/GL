8bce80a200259e930142b6175e4fa15823b7779d
wguoge1 <44798726+wguoge1@users.noreply.github.com>
2018-11-28 21:36:36 +0100

    Create main.c

---
+++ src/main.c
@@ -0,0 +1,34 @@
+// Copyright (c) 2018 Antoine Tran Tan
+//
+
+#include "my_header.h"
+#include "TP_lib_suite.h"
+void initialiserTirage(void);
+short tirerNumero(void);
+
+int main(void)
+{
+unsigned char joueur[6]={22,34,9,17,33,2},gagnante[6],valeur=0,k,i,n,q;
+for(q=0;q<100;q++)
+{
+for(k=0;k<6;k++)
+{
+initialiserTirage();
+gagnante[k]=tirerNumero();
+}
+
+for(i=0;i<6;i++)
+{
+for(n=0;n<6;n++)
+{
+if(joueur[i]==gagnante[n])
+{
+valeur=valeur+1;
+}
+
+}
+}
+
+}
+return 0;
+}
