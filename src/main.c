bd7c065264360578a61b6dc941d714a973510178
wguoge1 <44798726+wguoge1@users.noreply.github.com>
2018-12-05 00:01:34 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,9 +1,27 @@
 //  Copyright (c) 2018 Antoine Tran Tan
 //
-
+#include <userint.h>
+#include <stdio.h>
 #include "my_header.h"
-int main(void)
 
+int main(void)
 {
-    return 0;
+char nom[100],prenom[100],chaine[100]="Je m'appelle WEI GUO et j'ai 19 ans.";
+unsigned long int annee,age;
+int a;
+printf("veuillez taper votre nom,SVP\n");
+scanf("%s",nom);
+
+printf("veuillez taper votre prenom,SVP\n"); 
+scanf("%s",prenom); 
+
+printf("veuillez taper votre annee de naissance,SVP\n"); 
+scanf("%lu",&annee);
+
+age=2018-annee;
+
+printf("%s\n",chaine);
+
+a=MessagePopup("votre profil",chaine);
+return 0;
 }
