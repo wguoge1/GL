35a4a9df21a33498fc20cefec0e93f53aaeefa31
wguoge1 <44798726+wguoge1@users.noreply.github.com>
2018-12-11 09:44:03 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,9 +1,20 @@
 //  Copyright (c) 2018 Antoine Tran Tan
 //
 
-#include "my_header.h"
+#include <ansi_c.h>
+
+#include <stdio.h>  
+#include <math.h> 
 int main(void)
 
 {
+  
+  int valeur,a,b,c;
+  printf("veuillez saisir vos 3 couleurs");
+   scanf("%d %d %d,&a,&b,&c");
+  valeur=(10*a+b)*pow(10,c);
+  printf("la valeur est :%d",valeur);
+  
+	
     return 0;
 }
